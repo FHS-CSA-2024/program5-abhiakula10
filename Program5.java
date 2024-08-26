ca2be7e79a3f03672fac12aa785c549651f7fcc2
abhiakula10 <abhinavraj8258@gmail.com>
2024-08-25 23:03:25 -0700

    Update Program5.java
    I finished program 5

--- Program5.java
+++ Program5.java
@@ -1,9 +1,26 @@
-//import stuff here?
-
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+public class Program5 {
+    static double average(int miles, int gallons){
+        double mg = (double) miles / gallons;
+        mg = Math.round(mg*Math.pow(10,1)); 
+        mg /=10;
+        return mg;
+    }
+    public static void main(String[] args){
+        int royale_miles = 286;
+        int royale_gallons = 9;
+        int koopa_miles = 412;
+        int koopa_gallons = 40;
+        int pipe_miles = 361;
+        int pipe_gallons = 18;
+        int band_miles = 161;
+        int band_gallons = 11;
+        
+        System.out.println("The average miles per gallon for the Mushroom Cup Prix Racer was");
+        System.out.println("The average miles per gallon for the Royale was " + average(royale_miles, royale_gallons) + "m/g");
+        System.out.println("The average miles per galln for the Koopa King was " + average(koopa_miles, koopa_gallons) + "m/g");
+        System.out.println("The average miles per gallon for the Pipe frame was " + average(pipe_miles, pipe_gallons) + "m/g");
+        System.out.println("The average miles per gallon for the Bandwagon was " + average(band_miles, band_gallons) + "m/g");
+        
+    }
+     
+}
